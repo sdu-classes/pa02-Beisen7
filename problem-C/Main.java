eaede7406a50d5628aee67123a02d8e4d387f070
Beisen <113160775+Beisen7@users.noreply.github.com>
2023-02-08 14:27:57 +0600

    Update Main.java

---
+++ problem-C/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Person rektor = new Person("Alimzhan Igenbayev","Abylai Khan, 1/1");
+        Staff staff = new Staff("Satbek Abdyldayev","USA","SDU", 1 );
+        Student student = new Student("Beisen Adilov", "SDU", "Computer Science", 1,1.0);
+        
+    }
+}
