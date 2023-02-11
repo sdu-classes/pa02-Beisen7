d940b5bb7a806f567726b866a9b3946598acfb9a
Beisen <113160775+Beisen7@users.noreply.github.com>
2023-02-11 12:38:14 +0600

    Update Animal.java

--- problem-A/Animal.java
+++ problem-A/Animal.java
@@ -4,7 +4,7 @@ public class Animal {
     public Animal(String name) {
         this.name = name;
     }
-
+    @Override
     public String toString() {
         return "Animal[" +
                 "name='" + name + '\'' +
